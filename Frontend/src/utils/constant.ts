fbb32cbf3e06f6f45cedba68bf37bc2fba2246ed
Prabhat-Dongare <prabhat@ungraytech.com>
2024-12-07 19:53:36 +0530

    Updated Backend API links

--- Frontend/src/utils/constant.ts
+++ Frontend/src/utils/constant.ts
@@ -1,3 +1,7 @@
 // export const API_BASE_URL = "http://localhost:3000/api/";
-export const API_BASE_URL = "https://task-management-app-backend-efib.onrender.com/api/";
+// For JS Backend
+// export const API_BASE_URL = "https://task-management-app-backend-efib.onrender.com/api/";
+
+// For TS Backend
+export const API_BASE_URL = "https://task-management-app-ts-backend.onrender.com";
 
